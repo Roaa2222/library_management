64cf71d44dbbfba4f980fb3357a5765637064c46
roaa mahmoud <roaa1602@gmail.com>
2025-01-08 22:46:50 +0200

    Update admin.py

--- library_project/library/admin.py
+++ library_project/library/admin.py
@@ -6,6 +6,7 @@ from .models import Transaction
 admin.site.register(Book)
 admin.site.register(Transaction)
 admin.site.site_header = 'Library_Management'
+admin.site.site_title = 'Library_Management'
 #admin.header.
 
 # Register your models here.
