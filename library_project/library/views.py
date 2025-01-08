5a0b42ba090331b838beedda08ef289a5a49d0b0
roaa mahmoud <roaa1602@gmail.com>
2025-01-08 23:18:26 +0200

    Update views.py

--- library_project/library/views.py
+++ library_project/library/views.py
@@ -20,32 +20,6 @@ class BookViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(books, many=True)
         return Response(serializer.data)
 
-    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
-    def search(self, request):
-        """Search books by title, author, or ISBN."""
-        title = request.query_params.get('title', None)
-        author = request.query_params.get('author', None)
-        isbn = request.query_params.get('isbn', None)
-        queryset = self.queryset
-
-        if title:
-            queryset = queryset.filter(title__icontains=title)
-        if author:
-            queryset = queryset.filter(author__icontains=author)
-        if isbn:
-            queryset = queryset.filter(isbn=isbn)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
-    @action(detail=False, methods=['get'])
-    def available(self, request):
-        books = Book.objects.filter(copies_available__gt=0)
-        serializer = self.get_serializer(books, many=True)
-        return Response(serializer.data)
-    
-    
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
