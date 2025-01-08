45213c09519bed8f7ce374284c87309547879dca
roaa mahmoud <roaa1602@gmail.com>
2025-01-08 22:38:23 +0200

    "done"

--- library_project/library/admin.py
+++ library_project/library/admin.py
@@ -5,6 +5,7 @@ from .models import Transaction
 
 admin.site.register(Book)
 admin.site.register(Transaction)
+admin.site.site_header = 'Library_Management'
 #admin.header.
 
 # Register your models here.
--- library_project/library/views.py
+++ library_project/library/views.py
@@ -7,16 +7,44 @@ from .models import Book, Transaction
 from .serializers import BookSerializer, TransactionSerializer, UserSerializer
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def available(self, request):
+        """View available books only."""
+        books = Book.objects.filter(copies_available__gt=0)
+        serializer = self.get_serializer(books, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def search(self, request):
+        """Search books by title, author, or ISBN."""
+        title = request.query_params.get('title', None)
+        author = request.query_params.get('author', None)
+        isbn = request.query_params.get('isbn', None)
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+        if isbn:
+            queryset = queryset.filter(isbn=isbn)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     @action(detail=False, methods=['get'])
     def available(self, request):
         books = Book.objects.filter(copies_available__gt=0)
         serializer = self.get_serializer(books, many=True)
         return Response(serializer.data)
+    
+    
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
